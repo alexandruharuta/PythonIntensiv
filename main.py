a4eaa4fd7d500f3fa00110168faf1ff4556d9613
Alexandru Haruta <alexandruharuta@outlook.com>
2020-05-09 15:42:01 +0300

    Numbers and Operations

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 print('Hello')
+
+x = 5
+y = 6
+z = 2
+
+
+#Числа
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(-x)
+print(abs(x))
+print(divmod(x, y))
+print(x**y)
+
+#Битовые операции
+
+print(x|y)
+print(x^y)
+print(x&y)
+print(x<<y)
+print(x>>y)
+print(~y)
